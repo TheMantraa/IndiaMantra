c64ad7736890909d4349bfb07147f2b0cdf5613a
TheMantra <themantra.ca@gmail.com>
2025-01-25 15:18:30 +0530

    op

--- frontend/src/pages/Recipes.jsx
+++ frontend/src/pages/Recipes.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
   const navigate = useNavigate();
+
   const handleRecipeClick = (id) => {
     navigate(`/recipes/${id}`);
   };
@@ -13,7 +14,11 @@ const Recipes = () => {
     axios
       .get(`${process.env.REACT_APP_BACKEND_URL}/recipe/getAllRecipe`)
       .then((response) => {
-        setRecipes(response.data);
+        // Assuming each recipe has a 'createdAt' field, which is a date string
+        const sortedRecipes = response.data.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
+        setRecipes(sortedRecipes);
       })
       .catch((error) => {
         console.error("Error fetching recipes:", error);
@@ -21,9 +26,9 @@ const Recipes = () => {
   }, []);
 
   return (
-    <div className="py-8 px-4">
+    <div className="py-8 px-4 bg-[#eeebe9]">
       <div className="max-w-6xl mx-auto">
-        <h1 className="text-4xl font-extrabold mb-8 text-[#3C0C1C] sm:text-left baskervville-regular">
+        <h1 className="text-2xl font-extrabold mb-8 text-[#3C0C1C] sm:text-left baskervville-regular">
           RECIPES
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-4">
@@ -37,15 +42,15 @@ const Recipes = () => {
                 <img
                   src={recipe.imageUrl}
                   alt={recipe.title}
-                  className="w-full h-52- sm:h-56 object-cover rounded-md group-hover:scale-110 transition-transform duration-700"
+                  className="w-full h-52 sm:h-56 object-cover rounded-md group-hover:scale-110 transition-transform duration-700"
                 />
               </div>
               <div className="my-4">
-                <h2 className="sm:text-2xl lg:text-3xl font-bold text-gray-800 group-hover:underline cursor-pointer">
+                <h2 className="sm:text-xl lg:text-2xl font-bold text-gray-800 group-hover:underline cursor-pointer">
                   {recipe.title}
                 </h2>
-                <p className="text-xl sm:text-md text-gray-600 mt-3">
-                  {recipe.introduction}
+                <p className="text-md sm:text-md text-gray-600 mt-3">
+                  {recipe.introduction.slice(0, 120)}..
                 </p>
               </div>
             </div>
